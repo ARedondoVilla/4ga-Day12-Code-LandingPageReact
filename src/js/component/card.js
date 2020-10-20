350c0e8e5612a263f305fb5ad20d12b72bec1667
ARedondoVilla <alexredondovilla@gmail.com>
2020-10-20 09:34:59 +0000

    Resolucion error Card

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export function Card(props) {
-	return <h1>Card</h1>;
+	return (
+		<div className="card">
+			<img src="..." className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">Some quick example.</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
 }
